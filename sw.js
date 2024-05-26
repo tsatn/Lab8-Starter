adad08682729c09bb48decaaa4c9398fdd12e770
tsatn <shtian@ucsd.edu>
2024-05-26 14:16:09 -0700

    completed main.js code

--- sw.js
+++ sw.js
@@ -3,16 +3,33 @@
 
 const CACHE_NAME = 'lab-8-starter';
 
-// Installs the service worker. Feed it some initial URLs to cache
+
+const urlsToCache = [
+  // List of URLs to cache initially
+  './index.html',
+  './styles/main.css',
+  './scripts/main.js',
+  './images/logo.png',
+  // Adding recipe JSON URLs from the RECIPE_URLS array in main.js
+  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json'
+];
+// Install event - Caches important files and recipe data
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
-      //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+        console.log('Opened cache and adding initial URLs');
+        // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
+        //            added to the cache when the ServiceWorker is installed
+        return cache.addAll(urlsToCache);
     })
   );
 });
+
 
 // Activates the service worker
 self.addEventListener('activate', function (event) {
