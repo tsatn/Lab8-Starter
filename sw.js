61a9fc1c341b4de5cf0356e17ac2a77bbfa6cd9d
tsatn <shtian@ucsd.edu>
2024-05-29 00:04:14 -0700

    completed

--- sw.js
+++ sw.js
@@ -2,15 +2,23 @@
 //         so do not move it next to the other scripts
 const CACHE_NAME = 'lab-8-starter';
 
-const urlsToCache = [
-  // List of URLs to cache initially
-  'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
-  'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
-  'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
-  'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
-  'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
-  'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json'
-];
+// Installs the service worker. Feed it some initial URLs to cache
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
+      //            added to the cache when the ServiceWorker is installed
+      return cache.addAll([
+        'https://adarsh249.github.io/Lab8-Starter/recipes/1_50-thanksgiving-side-dishes.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/2_roasting-turkey-breast-with-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/3_moms-cornbread-stuffing.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/4_50-indulgent-thanksgiving-side-dishes-for-any-holiday-gathering.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/5_healthy-thanksgiving-recipe-crockpot-turkey-breast.json',
+        'https://adarsh249.github.io/Lab8-Starter/recipes/6_one-pot-thanksgiving-dinner.json',
+      ]);
+    })
+  );
+});
 
 // Install event - Caches important files and recipe data
 self.addEventListener('install', event => {
